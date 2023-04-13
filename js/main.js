dd3425738324f5b3dc0d0feb66fb9a12c44e460e
geoblo <geoblo@naver.com>
2023-04-13 18:27:02 +0900

    2023-04-13 수업

--- js/main.js
+++ js/main.js
@@ -19,3 +19,21 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', ''); // HTML 속성을 추가하는 메소드
 });
+
+// 스크롤 시 전역 배지(고정 배너) 숨기기
+const badgeEl = document.querySelector('header .badges');
+
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY); // y축으로 얼마나 스크롤 했는지에 대한 수치
+
+  // 만약 y축으로 스크롤 한 수치가 500을 초과하면 배지 요소를 숨기고
+  // 그렇지 않으면 다시 보이기!!
+  if (window.scrollY > 500) {
+    // 배지 요소 숨기기!
+    badgeEl.style.display = 'none';
+  } else {
+    // 배지 요소 보이기!
+    badgeEl.style.display = 'block';
+  }
+});
